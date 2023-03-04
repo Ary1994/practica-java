f461f8e6735045366a6a029c7eab0605a61dc653
Ary <ariadna.des.garcia@gmail.com>
2023-03-03 21:46:35 -0300

    primeros ejemplos poo

---
+++ src/alura/Cuenta.java
@@ -0,0 +1,8 @@
+package alura;
+
+public class Cuenta {
+ double saldo;
+ int agencia;
+ int numero;
+ String titular;
+}
---
+++ src/alura/crearCuenta.java
@@ -0,0 +1,10 @@
+package alura;
+
+public class crearCuenta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
