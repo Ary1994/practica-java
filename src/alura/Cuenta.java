f8987ba519fbfbb5311d700821ce660b9f246395
Ary <ariadna.des.garcia@gmail.com>
2023-03-04 21:16:30 -0300

    primeros metodos

--- src/alura/Cuenta.java
+++ src/alura/Cuenta.java
@@ -5,4 +5,25 @@ public class Cuenta {
  int agencia;
  int numero;
  String titular;
+ 	public void depositar( double valor) {
+	 this.saldo=this.saldo+valor;
+ 	}
+ 	public boolean retirar( double valor) {
+	 if (this.saldo>=valor) {
+		 this.saldo=this.saldo-valor;
+		 return true;
+	 }else {
+		 return false;
+	 }
+ 	}
+ 	public boolean tranferir(double valor,Cuenta cuenta) {
+ 		if(this.saldo>=valor) {
+ 			this.retirar(valor);
+ 			cuenta.depositar(valor);
+ 			return true;
+ 		}
+ 		else {
+ 			return false;
+ 		}
+ 	}
 }
--- src/alura/crearCuenta.java
+++ src/alura/crearCuenta.java
@@ -4,7 +4,13 @@ public class crearCuenta {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Cuenta primeraCuenta=new Cuenta();
+		primeraCuenta.depositar(200);
+		Cuenta segundaCuenta=new Cuenta();
+		segundaCuenta.depositar(1000);
+		segundaCuenta.tranferir(500, primeraCuenta);
+		System.out.println(primeraCuenta.saldo);
+		System.out.println(segundaCuenta.saldo);
 	}
 
 }
